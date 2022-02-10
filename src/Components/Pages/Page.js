c4aa87e05d14151da4b8fec3e5371b0046567950
Suyash Kelvin Savant <61877180+suyashkelvinsavant@users.noreply.github.com>
2022-02-11 03:06:12 +0530

    test 2

--- src/Components/Pages/Page.js
+++ src/Components/Pages/Page.js
@@ -1,12 +1,22 @@
-import {useMoralis} from 'react-moralis';
+import {useMoralis, MoralisProvider} from 'react-moralis';
 import User from './User/User'
 import Home from './Home/Home'
+
+const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
+const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
 function Page(){
     const { isAuthenticated } = useMoralis();
     if(isAuthenticated){
-        return(<User/>);
+        return(
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+        <User/></MoralisProvider>
+        );
     }else{
-        return(<Home/>);
+        return(
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+        <Home/>
+        </MoralisProvider>
+        );
     }
 }
 export default Page;
